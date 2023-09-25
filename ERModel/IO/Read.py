0d896b83f57a583d614d86a4588bada4361528e5
sadra-f <sadrafouladi@gmail.com>
2023-09-26 01:45:32 +0330

    added code to read dataset and return dataset as Document models

---
+++ ERModel/IO/Read.py
@@ -0,0 +1,20 @@
+from ERModel.models.document import Document as Doc
+import re
+
+
+class Reader:
+    def __init__(self) -> None:
+        pass
+
+
+    def read_dataset(path):
+        res = []
+        try:
+            with open(path) as file:
+                for line in file:
+                    splitted = re.split(';', line)
+                    res.append(Doc(splitted[0], splitted[1]))
+        except Exception as e:
+            print(f"Exception : {e.with_traceback()}")
+            
+        return res
