eb1c6bf098d1df25b13272731bec549e43e492c3
sadra-f <sadrafouladi@gmail.com>
2023-09-26 13:06:18 +0330

    added dataset paths

---
+++ ERModel/Statics/Config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+#string paths
+TRAIN_DATASET_STR_PATH = '../dataset/train.txt'
+TEST_DATASET_STR_PATH = '../dataset/test.txt'
+# VAL_DATASET_STR_PATH = '../dataset/val.txt'
+#path objs
+TRAIN_DATASET_PATH = Path(TRAIN_DATASET_STR_PATH)
+TEST_DATASET_PATH = Path(TEST_DATASET_STR_PATH)
+# VAL_DATASET_PATH = Path(TRAIN_DATASET_STR_PATH)
