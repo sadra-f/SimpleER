d56ef519f65211388b131580f065a73f0bf0e803
sadra-f <sadrafouladi@gmail.com>
2023-09-27 20:20:21 +0330

    seperating dataset based on their emotion

--- ERModel/ERModel.py
+++ ERModel/ERModel.py
@@ -1,18 +1,32 @@
 from .models.document import Document as Doc
 from .IO.Read import Reader
 from .TFIDF import TFIDF
+import numpy as np
 
 class ERM:
     def __init__(self, dataset_path):
         self.dataset = Reader.read_dataset(dataset_path)
         self._emotion_set = None
         self._build_emotion_set()
-        tfidf = TFIDF(self.dataset)
+        self.tfidf = TFIDF(self.dataset)
+        self._seperate_by_emotion()
+        self.seperated_tfidf = None
         return
 
 
     def _build_emotion_set(self):
         self._emotion_set = set()
-        for i, emotion in enumerate(self.dataset):
-            self._emotion_set.add(emotion)
+        for i, doc in enumerate(self.dataset):
+            self._emotion_set.add(doc.emotion)
         return
+    
+
+    def _seperate_by_emotion(self):
+        tmp = dict([(emo, []) for emo in self._emotion_set])
+        for val in self._emotion_set:
+            for doc in self.dataset:
+                if doc.emotion == val:
+                    tmp[val].append(doc)
+        self.dataset = tmp
+
+    
