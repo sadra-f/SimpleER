031edfe67ab44bfb9391d71763e5c0564cb254e9
sadra-f <sadrafouladi@gmail.com>
2023-09-29 14:54:05 +0330

    now uses cosine similarity to compare a query

--- ERModel/TFIDF.py
+++ ERModel/TFIDF.py
@@ -3,6 +3,7 @@ import re
 from .statics.Stopwords import STOP_WORDS
 import numpy as np
 from math import log10
+from .CosineSimilarity import cosine_similarity
 
 class TFIDF:
     def __init__(self, docs:list[Doc]) -> None:
@@ -53,24 +54,29 @@ class TFIDF:
         self.idf = np.sum(np.ones_like(self._tf), 1, where=self._tf > 0)
         self.idf = np.divide(len(self.documents), self.idf)
         self.idf = np.log10(self.idf)
-        self.idf = np.reshape(self.idf, (len(self.idf), 1))
+        # self.idf = np.reshape(self.idf, (len(self.idf), 1))
         return
         
         
 
 
     def calculate_tfidf(self):
-        self.tfidf = np.multiply(self._tf, self.idf)
+        self.tfidf = np.transpose(np.multiply(np.transpose(self._tf), self.idf))
         return
     
 
-    def compare(self, doc:str):
-        new_doc_terms = self._extract_terms([doc])
-        new_doc_term_index = [i for i, trm in enumerate(self.terms) if trm in new_doc_terms]
-        result = np.zeros((1, len(self.documents)))
-        for i in new_doc_term_index:
-            for j, doc in enumerate(self.documents):
-                result[0][j] += self.tfidf[i][j]
+    def compare(self, new_doc:str):
+        new_doc_terms = self._extract_terms([new_doc])
+        new_doc_tf = np.zeros((len(self.terms)))
+        for i, trm in enumerate(new_doc_terms):
+            new_doc_tf[self.terms.index(trm)] = new_doc.count(trm)
+        new_doc_tf = np.add(new_doc_tf, np.ones_like(new_doc_tf))
+        new_doc_tf = np.log10(new_doc_tf)
+        new_doc_tf[new_doc_tf != 0] += 1
+        new_doc_tfidf = np.multiply(new_doc_tf, self.idf)
+        result = np.zeros((1,len(self.documents)))
+        for i, document in enumerate(self.documents):
+            result[0][i] = cosine_similarity(new_doc_tfidf, self.tfidf[:,i])
         
         return result
 
