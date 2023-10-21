8f024debdb52fec69ea8b9a30d10c09f9f04dcd3
sadra-f <sadrafouladi@gmail.com>
2023-10-21 18:24:53 +0330

    chnaged dataset path

--- ERModel/Statics/Config.py
+++ ERModel/Statics/Config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 #string paths
-TRAIN_DATASET_STR_PATH = '../dataset/train.txt'
-TEST_DATASET_STR_PATH = '../dataset/test.txt'
+TRAIN_DATASET_STR_PATH = 'dataset/train.txt'
+TEST_DATASET_STR_PATH = 'dataset/test.txt'
 # VAL_DATASET_STR_PATH = '../dataset/val.txt'
 #path objs
 TRAIN_DATASET_PATH = Path(TRAIN_DATASET_STR_PATH)
