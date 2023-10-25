5f4fb5fe67e8b19d1849fe48e405ec48c7c2efb3
sadra-f <sadrafouladi@gmail.com>
2023-10-25 22:02:36 +0330

    added comments

--- ERModel/CosineSimilarity.py
+++ ERModel/CosineSimilarity.py
@@ -1,11 +1,20 @@
 import numpy as np
 
 def cosine_similarity(vec1, vec2):
+    """
+        calculates and returns the cosine similarity between vec1 and vec2 using numpy methods
+    """
     return vector_dot_product(vec1, vec2) / (vector_magnitude(vec1) * vector_magnitude(vec2))
 
 
 def vector_dot_product(vec1, vec2):
+    """
+        calculates and returns the dot product of vec1 and vec2 using numpy methods
+    """
     return np.dot(vec1, vec2)
 
 def vector_magnitude(vec):
+    """
+        calculates and returns the magnitude of vec
+    """
     return np.sqrt(vec.dot(vec))
--- ERModel/models/document.py
+++ ERModel/models/document.py
@@ -1,4 +1,17 @@
 class Document:
+    """
+        Document class meant to hold the text and its respective emotion
+
+        variables
+            _id: id counter used to number the inputs
+            string: the string which is to be stored
+            emotion: the emotion/class representing the string/text
+            id: the id of the Document starts from 0 incremented by 1 on each object creation
+        
+        inputs
+            string: the string which is to be stored
+            emotion: the emotion/class representing the string/text
+    """
     _id = 0
     def __init__(self, string, emotion):
         self.string = string
