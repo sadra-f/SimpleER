a0135d0c4c0a5f49396c3f20162f8e44093c269d
sadra-f <sadrafouladi@gmail.com>
2023-10-25 17:15:44 +0330

    now holds use of different functionalities of the code

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from ERModel.Evaluate import Evaluator
 from datetime import datetime
 
 m = ERM()
-# m.train(TRAIN_DATASET_PATH)
-# m.save_model()
+m.train(TRAIN_DATASET_PATH)
+m.save_model()
 m = ERM.load_model()
 m.predict('today is a good day and that makes me feel great')
 eval = Evaluator(m, TEST_DATASET_PATH)
